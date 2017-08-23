12a2d9e8d1c62c0a87e39bd5050917da29988a20
pachet <pachet@burninggarden.com>
2017-08-23 16:09:22 +0000

    Stub out more ActiveRecord::Base class methods

--- activerecord/lib/active_record/base.rb
+++ activerecord/lib/active_record/base.rb
@@ -5,6 +5,8 @@ module ActiveRecord
 
   class Base
 
+    @@belongs_to_required_by_default = false
+
     def Base.abstract_class=(abstract_class)
       @@abstract_class = abstract_class
     end
@@ -16,6 +18,11 @@ module ActiveRecord
 
     def Base.table_name
       @@table_name
+    end
+
+
+    def Base.sequence_name=(sequence_name)
+      @@sequence_name = sequence_name
     end
 
 
@@ -41,14 +48,71 @@ module ActiveRecord
       @@inheritance_column = inheritance_column
     end
 
+    def Base.ignored_columns=(ignored_columns)
+      @@ignored_columns = ignored_columns
+    end
+
+    def Base.cache_timestamp_format=(cache_timestamp_format)
+      @@cache_timestamp_format = cache_timestamp_format
+    end
+
+    def Base.belongs_to_required_by_default=(belongs_to_required_by_default)
+      @@belongs_to_required_by_default = belongs_to_required_by_default
+    end
+
+    def Base.belongs_to_required_by_default
+      @@belongs_to_required_by_default
+    end
+
+    def Base.primary_key=(primary_key)
+      @@primary_key = primary_key
+    end
 
 
 
-    def Base.default_scope
+
+    def Base.default_scope(*splat)
       # ?
     end
 
-    def Base.serialize(symbol)
+    def Base.serialize(*splat)
+    end
+
+    def Base.to_param(symbol)
+    end
+
+    def Base.define_attribute_method(string)
+    end
+
+    def Base.where(*splat)
+    end
+
+    def Base.includes(*splat)
+    end
+
+    def Base.attribute(*splat)
+    end
+
+    def Base.attr_readonly(*splat)
+    end
+
+
+
+    # Validation:
+
+    def Base.validate(*splat)
+    end
+
+    def Base.validates_presence_of(symbol)
+    end
+
+    def Base.validates_uniqueness_of(*splat)
+    end
+
+    def Base.validates_inclusion_of(*splat)
+    end
+
+    def Base.validates_length_of(*splat)
     end
 
 
@@ -56,10 +120,31 @@ module ActiveRecord
 
     # Hooks:
 
-    def Base.after_create
+    def Base.after_initialize(*splat)
     end
 
-    def Base.after_save
+    def Base.before_create(*splat)
+    end
+
+    def Base.after_create(*splat)
+    end
+
+    def Base.before_validation(*splat)
+    end
+
+    def Base.after_find(*splat)
+    end
+
+    def Base.before_update(*splat)
+    end
+
+    def Base.before_save(*splat)
+    end
+
+    def Base.after_save(*splat)
+    end
+
+    def Base.before_destroy(*splat)
     end
 
 
@@ -74,10 +159,17 @@ module ActiveRecord
     def Base.has_and_belongs_to_many(*splat)
     end
 
+    def Base.has_one(*splat)
+    end
+
+
     def Base.scope(identifier, block)
     end
 
-    def Base.has_one(*splat)
+    def Base.composed_of(*splat)
+    end
+
+    def Base.accepts_nested_attributes_for(*splat)
     end
 
 
