75b46bdf54841935aa4c9527b707b3cbe23aad2a
pachet <pachet@burninggarden.com>
2017-08-23 14:38:25 +0000

    Add necessary ActiveSupport dependencies for running ActiveRecord tests

---
+++ activerecord/lib/active_record.rb
@@ -0,0 +1,4 @@
+
+class ActiveRecord
+
+end
---
+++ activesupport/lib/active_support/core_ext/date/zones.rb
@@ -0,0 +1,9 @@
+
+# frozen_string_literal: true
+
+require "date"
+require_relative "../date_and_time/zones"
+
+class Date
+  include DateAndTime::Zones
+end
